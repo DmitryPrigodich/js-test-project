1559e4f656ffe2b594f1306b89f45b37efe670a1
Dmitrijs_Prigodics <dmitrijs.prigodics@externals.adidas.com>
2022-10-04 20:11:08 +0300

    The Very First Commit

---
+++ test/numbers-validator/isNumberEven.spec.js
@@ -0,0 +1,21 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable indent */
+/* eslint-disable max-len */
+/* eslint-disable require-jsdoc */
+
+import { NumbersValidator } from '../../app/NumbersValidator';
+import { expect } from 'chai';
+
+describe('isNumberEven positive tests', () => {
+    let validator;
+
+    beforeEach(() => {
+        validator = new NumbersValidator();
+    });
+
+    it('should return true when provided with an even number', () => {
+        const validationResults = validator.isNumberEven(4);
+        expect(validationResults).to.be.equal(true);
+    });
+});
